0566b4e444d7db01938fddff550cdbde4663e0d1
yuvasri <madhavan@trisentrix.com>
2026-03-16 18:14:42 +0530

    build.dradle

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import codePush from "@revopush/react-native-code-push"; // Use the Revopush SDK
 
-AppRegistry.registerComponent(appName, () => App);
+const codePushOptions = { 
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, 
+  installMode: codePush.InstallMode.ON_NEXT_RESUME,
+  mandatoryInstallMode: codePush.InstallMode.IMMEDIATE 
+};
+
+// Register the wrapped version of your app
+AppRegistry.registerComponent(appName, () => codePush(codePushOptions)(App));
